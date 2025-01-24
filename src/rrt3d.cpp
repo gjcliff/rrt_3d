bfd1cff389831955d259d5ee5dabbc1adc99767b
Graham Clifford <gjcliff@gmail.com>
2025-01-24 12:14:23 -0500

    RRT node should be mostly done
    I haven't written the logic to change the color of the goal path yet,
    but I can do that once I confirm the base algo is working and that
    things are showing up in rviz.

--- src/rrt3d.cpp
+++ src/rrt3d.cpp
@@ -7,8 +7,11 @@
 
 #include "geometry_msgs/msg/point.hpp"
 #include "geometry_msgs/msg/vector3.hpp"
-#include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
+
+#include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
 
@@ -16,16 +19,24 @@ struct RRT_Node {
 public:
   RRT_Node() {}
   RRT_Node(geometry_msgs::msg::Point coord, std::shared_ptr<RRT_Node> parent)
-    : coord(coord), parent(parent)
+    : dist(0), coord(coord), parent(parent)
   {
   }
+  RRT_Node(int dist, geometry_msgs::msg::Point coord,
+           std::shared_ptr<RRT_Node> parent)
+    : dist(dist), coord(coord), parent(parent)
+  {
+  }
+  int dist;
   geometry_msgs::msg::Point coord;
   std::shared_ptr<RRT_Node> parent;
 };
 
 class RRT3D : public rclcpp::Node {
 public:
-  RRT3D() : Node("rrt3d"), found_goal_(false), rd(), gen(rd())
+  RRT3D()
+    : Node("rrt3d"), found_goal_(false), rd(), gen(rd()), node_count_(0),
+      arrow_count_(0)
   {
     // declare parameters
     declare_parameter<std::vector<double>>("start_coord", {0.0, 0.0, 0.0});
@@ -56,11 +67,14 @@ public:
 
     // initialize some variables
     std::shared_ptr<RRT_Node> start =
-      std::make_shared<RRT_Node>(start_coord, nullptr);
+      std::make_shared<RRT_Node>(start_coord_, nullptr);
     nodes.push_back(start);
 
     // initialize publishers, subscribers, etc.
-    publisher_ = create_publisher<std_msgs::msg::String>("topic", 10);
+    node_publisher_ =
+      create_publisher<visualization_msgs::msg::MarkerArray>("nodes", 10);
+    arrow_publisher_ =
+      create_publisher<visualization_msgs::msg::MarkerArray>("arrows", 10);
     timer_ = create_wall_timer(500ms, std::bind(&RRT3D::run, this));
   }
 
@@ -72,9 +86,47 @@ public:
                      std::pow(start.z - end.z, 2));
   }
 
+  std::shared_ptr<visualization_msgs::msg::Marker>
+  create_sphere_marker(std::shared_ptr<RRT_Node> node)
+  {
+    visualization_msgs::msg::Marker marker;
+    marker.header.stamp = get_clock()->now();
+    marker.header.frame_id = "world";
+    marker.id = node_count_++;
+    marker.type = visualization_msgs::msg::Marker::SPHERE;
+    marker.action = visualization_msgs::msg::Marker::ADD;
+    marker.pose.position.x = node->coord.x;
+    marker.pose.position.y = node->coord.y;
+    marker.pose.position.z = node->coord.z;
+    marker.color.r = 1.0;
+    marker.color.a = 1.0;
+    marker.scale.x = 0.5;
+    marker.scale.y = 0.5;
+    marker.scale.z = 0.5;
+
+    return std::make_shared<visualization_msgs::msg::Marker>(marker);
+  }
+
+  std::shared_ptr<visualization_msgs::msg::Marker>
+  create_arrow_marker(std::shared_ptr<RRT_Node> start,
+                      std::shared_ptr<RRT_Node> end)
+  {
+    visualization_msgs::msg::Marker marker;
+    marker.header.stamp = get_clock()->now();
+    marker.header.frame_id = "world";
+    marker.id = arrow_count_++;
+    marker.type = visualization_msgs::msg::Marker::ARROW;
+    marker.action = visualization_msgs::msg::Marker::ADD;
+    marker.points = {start->coord, end->coord};
+    marker.color.r = 1.0;
+    marker.color.a = 1.0;
+
+    return std::make_shared<visualization_msgs::msg::Marker>(marker);
+  }
+
   void run()
   {
-    if (!found_goal_){
+    if (!found_goal_) {
       // pick a random point in the workspace
       geometry_msgs::msg::Point rpoint;
       std::uniform_real_distribution<> ux(-world_bounds_.x, world_bounds_.x);
@@ -99,18 +151,27 @@ public:
       // create unit vector
       geometry_msgs::msg::Vector3 unit_vector;
       unit_vector.x = (rpoint.x - closest_node->coord.x) /
-        euclidean_distance(rpoint, closest_node->coord);
+                      euclidean_distance(rpoint, closest_node->coord);
       unit_vector.y = (rpoint.y - closest_node->coord.y) /
-        euclidean_distance(rpoint, closest_node->coord);
+                      euclidean_distance(rpoint, closest_node->coord);
       unit_vector.z = (rpoint.z - closest_node->coord.z) /
-        euclidean_distance(rpoint, closest_node->coord);
+                      euclidean_distance(rpoint, closest_node->coord);
 
       geometry_msgs::msg::Point new_point;
       new_point.x = closest_node->coord.x + unit_vector.x;
       new_point.y = closest_node->coord.y + unit_vector.y;
       new_point.z = closest_node->coord.z + unit_vector.z;
 
-      nodes.push_back(std::make_shared<RRT_Node>(new_point, closest_node));
+      std::shared_ptr<RRT_Node> new_node = std::make_shared<RRT_Node>(
+        closest_node->dist + 1, new_point, closest_node);
+      nodes.push_back(std::make_shared<RRT_Node>(closest_node->dist + 1,
+                                                 new_point, closest_node));
+      node_markers_.markers.push_back(*create_sphere_marker(new_node));
+      arrow_markers_.markers.push_back(
+        *create_arrow_marker(closest_node, new_node));
+
+      node_publisher_->publish(node_markers_);
+      arrow_publisher_->publish(arrow_markers_);
 
       if (euclidean_distance(new_point, goal_coord_) < 0.2) {
         found_goal_ = true;
@@ -121,8 +182,13 @@ public:
 
 private:
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr
+    node_publisher_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr
+    arrow_publisher_;
 
+  visualization_msgs::msg::MarkerArray node_markers_;
+  visualization_msgs::msg::MarkerArray arrow_markers_;
   geometry_msgs::msg::Point world_bounds_;
   geometry_msgs::msg::Point start_coord_;
   geometry_msgs::msg::Point goal_coord_;
@@ -134,7 +200,8 @@ private:
 
   std::vector<std::shared_ptr<RRT_Node>> nodes; // start with this, can optimize
 
-  // 847-491-7227
+  int node_count_;
+  int arrow_count_;
 };
 
 int main(int argc, char *argv[])
